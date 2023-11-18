4d093542abfb0ee37f8935b8876d1cba20593426
David <52141166+sdavidbastos@users.noreply.github.com>
2023-11-18 23:22:07 +0000

    :bricks: env

--- src/routes/market-routes.js
+++
@@ -1,9 +0,0 @@
-const { ExpressAdapter } = require('../adapters/express.js');
-const { authUseCase, createMarketUseCase, listMarketProductsUseCase } = require("../domain")
-
-const marketRoutes = require('express').Router();
-
-marketRoutes.use(ExpressAdapter.execute(authUseCase))
-marketRoutes.post('/market', ExpressAdapter.execute(createMarketUseCase));
-marketRoutes.get('/market/:marketId?/:id?', ExpressAdapter.execute(listMarketProductsUseCase))
-module.exports = { marketRoutes }
--- src/routes/product-routes.js
+++
@@ -1,9 +0,0 @@
-const { ExpressAdapter } = require('../adapters/express.js');
-const { createProductUseCase, authUseCase } = require('../domain/index.js');
-
-const productRoutes = require('express').Router();
-
-productRoutes.use(ExpressAdapter.execute(authUseCase))
-productRoutes.post('/product', ExpressAdapter.execute(createProductUseCase));
-
-module.exports = { productRoutes }
